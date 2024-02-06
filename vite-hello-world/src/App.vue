646494b06641398a5d015b172d40b7b2927d25ff
francescolazzarin <francesco.lazzarin1996@gmail.com>
2024-02-06 16:14:54 +0100

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import AppTitle from './components/AppTitle.vue'
+
+
+  export default{
+    components:{
+      AppTitle
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <AppTitle/>
+  </header>
+  
+  
+
+</template>
+
+<style>
+
+</style>
---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+    export default{
+        name:"AppTitle"
+    }
+
+</script>
+
+<template>
+
+    <h1>La mia prima app con Vite!</h1>
+
+</template>
+
+<style scoped>
+    h1{
+        text-align: center;
+        text-decoration: underline;
+        color: blue;
+    }
+</style>
